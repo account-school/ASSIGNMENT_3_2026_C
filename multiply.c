5bf006669f2a1a96be7f864bf910a1368f73537f
account-school <nberg1@ucsc.edu>
2026-05-03 23:39:35 +0000

    problems 1-->4.5

--- multiply.c
+++ multiply.c
@@ -2,24 +2,114 @@
 
 #include <stdio.h>
 
+void print_matrice(int arr[][99], int rows, int cols);
+
 int main() {
     int n, m; //store the size of a
     printf("Enter the number of rows and columns for matrix a: ");
     /* TODO: read the size of a */
     
-
+    scanf("%i %i", &n, &m);
 
     int p, q; //store the size of b
     printf("Enter the number of rows and columns for matrix b: ");
     /* TODO: read the size of b */
     
+    scanf("%i %i",&p, &q);
 
-
-    int a[n][m], b[p][q], c[n][q]; //declaring 3 matrices
+    int a[n][m], b[p][q], c[n][q]; //declaring 3 matrices, automatically declares size a n by q, appropriately.
 
     /* TODO: if the sizes are compatible, read a and b from the user, multiply them and store the result in c, and print all three matrices.
              if the sizes are not compatible, print an error and exit the program. Return -1. 
              your output format must match the sample output shown in the pdf. */
+
+    // sizes are compatible if m == p
+
+    if (m != p) {
+        printf("The given matrix sizes are not compatible!");
+        return 0;
+    }
+
+    // implict else via return 0 earlier in if conditional.
+
+    // get matrix input
+
+    printf("Enter %d values for matrix a: ",n*m);
+    
+    
+    int input, j = 0; // input is uninitialized, j = 0;
+    while (scanf("%i", &input) == 1 &&  j < (n*m - 1)) {
+        a[j/m][j%m] = input;
+         //printf("a[%d][%d] = %d",j/m,j%m,input);
+        j++;
+    }
+    
+    a[j/m][j%m] = input;
+    
+
+    printf("Enter %d values for matrix b: ",p*q);
+    
+    
+    int input2, j2 = 0; // input2 is uninitialized, j2 = 0;
+    while (scanf("%i", &input2) == 1 && j2 < (p * q - 1)) {
+        b[j2/q][j2%q] = input2;
+        j2++;
+    }
+
+
+    b[j2/q][j2%q] = input2;
+
+
+    // display matrices
+
+    printf("Matrix a:\n");
+
+
+    for (int r = 0; r < n; r++) {
+        for (int c = 0; c < m; c++) {
+            printf("%d ",a[r][c]);
+        }
+        printf("\n");
+    }
+
+
+    printf("Matrix b:\n");
+
+
+    for (int r = 0; r < p; r++) {
+        for (int c = 0; c < q; c++) {
+            printf("%d ",b[r][c]);
+        }
+        printf("\n");
+    }
+
+
+
+
+
+    // now actually mutliply them
+    
+    for (int rows1 = 0; rows1 < n; rows1++) {
+        // multiply matrice: 1x3 3x1 (conceptual example (not in code) from Organic Chemistry Tutor),
+        // then replace [0] with index [rows1] for each row.
+        for (int cols2= 0; cols2 < q; cols2++) {
+            c[rows1][cols2] = 0;
+            for (int cols1 = 0; cols1 < m; cols1++) {
+                c[rows1][cols2] += a[rows1][cols1] * b[cols1][cols2];
+            }
+        }
+    }
+    // display them
+
+        printf("Matrix c:\n");
+
+
+    for (int r = 0; r < n; r++) {
+        for (int z = 0; z < q; z++) {
+            printf("%d ",c[r][z]);
+        }
+        printf("\n");
+    }
 
 
 
--- sort.c
+++ sort.c
@@ -19,8 +19,9 @@ int main() {
         printf("%d ", arr[i]);
     }
     printf("\n");
-
-    merge_sort(arr, 0, n - 1);
+    
+    merge(arr,0,n/2,n-1);
+    //merge_sort(arr, 0, n - 1);
 
     printf("Sorted array: ");
     for (int i = 0; i < n; i++) {
@@ -31,10 +32,67 @@ int main() {
     return 0;
 }
 
+
+void print_arr(int arr[], int left, int right) {
+    int len = right - left + 1;
+    printf("\[");
+    for (int n = 0; n < len-1; n++) {
+        printf("%d ",arr[n]);
+    }
+    printf("%d\]\n",arr[len-1]);
+}
 void merge_sort(int arr[], int left, int right) {
     /* TODO: Implement merge sort by using divide and conquer recursively. This function should call the merge sub-routine. */ 
+    
 }
 
 void merge(int arr[], int left, int mid, int right) {
     /* TODO: Implement the merge logic. */
+    int leftlen = mid-left+1;
+    int rightlen = right-mid+1;
+    int len = right-left+1;
+    // using psuedocode on slide 10 as massive helper
+    //printf("leftLen: %d, rightLen: %d len: %d ",leftlen, rightlen, len);
+    int tempArr[len];
+    int i = left;
+    int j = mid;
+    int k = 0;
+    
+    while (k < len && i < mid && j < len) {
+        //printf("mid: %d, len: %d\n",mid, len);
+        //printf("arr\[%d\] ? arr\[%d\]\n", i,j);
+        //printf("%d ? %d\n", arr[i],arr[j]);
+        if (arr[i] <= arr[j]) {
+            tempArr[k] = arr[i];
+            i++;
+        } else {
+            tempArr[k] = arr[j];
+            j++;           
+        }
+
+        //printf("temp arr::");
+        //print_arr(tempArr,left,right);
+        k++;
+    }
+    // implicit if nnot finished, copy values until. at this point one of them (i or j) must be completed by this point
+    printf("i: %d, j: %d, k: %d, mid: %d, len: %d\n",i,j,k, mid, len);
+    while (i < mid) {
+        tempArr[k] = arr[i];
+        i++;
+        k++;
+        //printf("temp arr::");
+        //print_arr(tempArr,left,right);
+    }
+    while (j < len) {
+        tempArr[k] = arr[j];
+        j++;
+        k++;
+        //printf("temp arr::");
+        //print_arr(tempArr,left,right);
+    }
+    // copy it into arr[]
+
+    for (int n = 0; n <= len; n++) {
+        arr[n] =  tempArr[n];
+    }
 }
